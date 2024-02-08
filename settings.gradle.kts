553e1e0b2a260cfe735b77af2b6226596d1cdc08
ShaiNachum <100000990+ShaiNachum@users.noreply.github.com>
2024-02-08 21:40:13 +0200

    first commit (#1)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "avoid_the_obsticle_game"
+include(":app")
+ 
